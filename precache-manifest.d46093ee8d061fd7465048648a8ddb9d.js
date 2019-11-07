e679b710aeba115b272a59ae7234b8bf8c0916c6
x3mprog <x3mprog@gmail.com>
2019-11-07 14:38:00 +0300

    Updates

---
+++ precache-manifest.d46093ee8d061fd7465048648a8ddb9d.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17d1d9877becbc16b220ddcb11ab4824",
+    "url": "/webapp-cjkh-frontend/index.html"
+  },
+  {
+    "revision": "5110046ad73425d3c343",
+    "url": "/webapp-cjkh-frontend/static/css/main.6a6792c9.chunk.css"
+  },
+  {
+    "revision": "bc0b9e5b75e860f3666b",
+    "url": "/webapp-cjkh-frontend/static/js/2.a2726108.chunk.js"
+  },
+  {
+    "revision": "5110046ad73425d3c343",
+    "url": "/webapp-cjkh-frontend/static/js/main.467841ae.chunk.js"
+  },
+  {
+    "revision": "1b2ec7510e74d83e186e",
+    "url": "/webapp-cjkh-frontend/static/js/runtime-main.0f9cd02e.js"
+  },
+  {
+    "revision": "4e9aa5fb7261cc668a0010c981254f46",
+    "url": "/webapp-cjkh-frontend/static/media/Seravek-Bold.4e9aa5fb.ttf"
+  },
+  {
+    "revision": "561175f04ae54248eb2f64ba156648ed",
+    "url": "/webapp-cjkh-frontend/static/media/Seravek-Bold.561175f0.svg"
+  },
+  {
+    "revision": "ba839e4ed791b94291ccae1cec9f58df",
+    "url": "/webapp-cjkh-frontend/static/media/Seravek-Bold.ba839e4e.woff2"
+  },
+  {
+    "revision": "d0b0ed943dcef7942cc200e6652246db",
+    "url": "/webapp-cjkh-frontend/static/media/Seravek-Bold.d0b0ed94.woff"
+  },
+  {
+    "revision": "e1fd97e10f485634c65d5ba375713403",
+    "url": "/webapp-cjkh-frontend/static/media/Seravek-Bold.e1fd97e1.eot"
+  },
+  {
+    "revision": "55b215c1f4f60864956c1aefee53408e",
+    "url": "/webapp-cjkh-frontend/static/media/Seravek-Light.55b215c1.woff"
+  },
+  {
+    "revision": "8b344848c20dbe372338108bcee2d066",
+    "url": "/webapp-cjkh-frontend/static/media/Seravek-Light.8b344848.eot"
+  },
+  {
+    "revision": "d08a61bf29607f05c0689be849f69e20",
+    "url": "/webapp-cjkh-frontend/static/media/Seravek-Light.d08a61bf.ttf"
+  },
+  {
+    "revision": "53d628fce1598bcce6a5e54f4cdf5372",
+    "url": "/webapp-cjkh-frontend/static/media/Seravek-Medium.53d628fc.woff2"
+  },
+  {
+    "revision": "589379f84981bdc7cbcacbc2324dbe78",
+    "url": "/webapp-cjkh-frontend/static/media/Seravek-Medium.589379f8.eot"
+  },
+  {
+    "revision": "a273b0b36f860462fa65a4e753fd3a8b",
+    "url": "/webapp-cjkh-frontend/static/media/Seravek-Medium.a273b0b3.woff"
+  },
+  {
+    "revision": "ccc03143ce12ae5dba7dd3aef7428a43",
+    "url": "/webapp-cjkh-frontend/static/media/Seravek-Medium.ccc03143.svg"
+  },
+  {
+    "revision": "e596b19025e4b3f29ebf2b966b132fec",
+    "url": "/webapp-cjkh-frontend/static/media/Seravek-Medium.e596b190.ttf"
+  },
+  {
+    "revision": "0b44c7b5bfb708d9827a03899b8a74fe",
+    "url": "/webapp-cjkh-frontend/static/media/Seravek.0b44c7b5.ttf"
+  },
+  {
+    "revision": "1792ab8d9ee88a3c0c75db52cd4c030f",
+    "url": "/webapp-cjkh-frontend/static/media/Seravek.1792ab8d.svg"
+  },
+  {
+    "revision": "b351c2fbe2c08cd78728ec1d89c7c8b4",
+    "url": "/webapp-cjkh-frontend/static/media/Seravek.b351c2fb.woff"
+  },
+  {
+    "revision": "d7cd9825fea477254ac67e1302d6c95c",
+    "url": "/webapp-cjkh-frontend/static/media/Seravek.d7cd9825.eot"
+  },
+  {
+    "revision": "807f94bc052895f24c3d5ab406998732",
+    "url": "/webapp-cjkh-frontend/static/media/banner.807f94bc.png"
+  },
+  {
+    "revision": "848e7571a894bda0be1773f0e6a98658",
+    "url": "/webapp-cjkh-frontend/static/media/feedback-example.848e7571.png"
+  },
+  {
+    "revision": "6896f59be9a16f790dd813d53c8f5faa",
+    "url": "/webapp-cjkh-frontend/static/media/fontello.6896f59b.eot"
+  },
+  {
+    "revision": "a425a277fcf195362af76525590a8a63",
+    "url": "/webapp-cjkh-frontend/static/media/fontello.a425a277.woff"
+  },
+  {
+    "revision": "c05e845f6ee9b32758bd09c862b327a2",
+    "url": "/webapp-cjkh-frontend/static/media/fontello.c05e845f.woff2"
+  },
+  {
+    "revision": "cdcbd5ef6a1fc2cc37dd48a8e1cda323",
+    "url": "/webapp-cjkh-frontend/static/media/fontello.cdcbd5ef.svg"
+  },
+  {
+    "revision": "ee275448880c54f64d21c4793c987fee",
+    "url": "/webapp-cjkh-frontend/static/media/fontello.ee275448.ttf"
+  },
+  {
+    "revision": "a267e5fdb34086b851ba47586636708e",
+    "url": "/webapp-cjkh-frontend/static/media/more_banner.a267e5fd.png"
+  },
+  {
+    "revision": "862f1e833b2c2d8381754c6b852cfbd5",
+    "url": "/webapp-cjkh-frontend/static/media/pavilion1.862f1e83.png"
+  },
+  {
+    "revision": "b0089070445dc86d706f5db1d6df8bb1",
+    "url": "/webapp-cjkh-frontend/static/media/pavilion2.b0089070.png"
+  },
+  {
+    "revision": "9382d5567cabe53b8ab97116a6c7eb15",
+    "url": "/webapp-cjkh-frontend/static/media/pavilion3.9382d556.png"
+  },
+  {
+    "revision": "e740283f5d294353f7c6d91ec09be37d",
+    "url": "/webapp-cjkh-frontend/static/media/pavilion4.e740283f.png"
+  },
+  {
+    "revision": "7d8405acc09dd9f97dfa7e1b18ea7c8b",
+    "url": "/webapp-cjkh-frontend/static/media/pavilion5.7d8405ac.png"
+  },
+  {
+    "revision": "a97b3da03ea436b3d459fd21f39cc962",
+    "url": "/webapp-cjkh-frontend/static/media/works_banner.a97b3da0.png"
+  }
+]);
